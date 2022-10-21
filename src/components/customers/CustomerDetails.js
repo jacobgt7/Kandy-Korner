2d4ea876961c473c6fe0287cf47644e0de334c65
Jacob Thomas <jacgretho@gmail.com>
2022-10-21 16:11:09 -0500

    added purchase functionality and abitlity to update customer loyalty number

--- src/components/customers/CustomerDetails.js
+++ src/components/customers/CustomerDetails.js
@@ -6,23 +6,51 @@ export const CustomerDetails = () => {
 
     const { userId } = useParams()
 
+
     useEffect(
         () => {
             fetch(`http://localhost:8088/customers?userId=${userId}&_expand=user`)
                 .then(res => res.json())
                 .then(customerData => {
-                    setCustomer(customerData)
+                    setCustomer(customerData[0])
                 })
         },
         []
     )
 
+    const updateButtonHandler = () => {
+        fetch(`http://localhost:8088/customers/${customer.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(customer)
+        }
+        )
+            .then(res => res.json())
+            .then(() => {
+
+            })
+
+    }
+
     return <>
         <h1>Customer Details</h1>
         <div>
-            <h3>{customer[0]?.user?.name}</h3>
-            <div>Email: {customer[0]?.user?.email}</div>
-            <div>Loyalty Number: {customer[0]?.loyaltyNumber}</div>
+            <h3>{customer?.user?.name}</h3>
+            <div>Email: {customer?.user?.email}</div>
+            <label htmlFor="loyaltyNumber"> Loyalty Number
+                <input type="text" id="loyaltyNumber" onChange={
+                    (e) => {
+                        const copy = { ...customer }
+                        copy.loyaltyNumber = parseInt(e.target.value)
+                        setCustomer(copy)
+                    }
+                } value={customer.loyaltyNumber} />
+                <button onClick={updateButtonHandler}>Update</button>
+            </label>
         </div>
     </>
 }
+
+
--- src/components/products/FindProductResult.js
+++ src/components/products/FindProductResult.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react"
 export const FindProductResult = ({ searchTerms }) => {
     const [products, setProducts] = useState([])
     const [filteredProducts, setFilteredProducts] = useState([])
+    const [currentCustomer, setCurrentCustomer] = useState({})
+
+    const localKandyUser = localStorage.getItem("kandy_user")
+    const kandyUserObject = JSON.parse(localKandyUser)
 
     useEffect(
         () => {
@@ -11,6 +15,13 @@ export const FindProductResult = ({ searchTerms }) => {
                 .then(productsData => {
                     setProducts(productsData)
                 })
+
+            fetch(`http://localhost:8088/customers?userId=${kandyUserObject.id}`)
+                .then(res => res.json())
+                .then((singleCustomerArray) => {
+                    setCurrentCustomer(singleCustomerArray[0])
+                })
+
         },
         []
     )
@@ -23,13 +34,36 @@ export const FindProductResult = ({ searchTerms }) => {
         [searchTerms]
     )
 
+
+    const purchaseButtonHandler = (currentProductId) => { //To do: create a new component to list "My Orders"
+        const purchaseObject = {
+            customerId: currentCustomer.id,
+            productId: currentProductId,
+            quantity: 1
+        }
+
+        fetch(`http://localhost:8088/purchases`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(purchaseObject)
+        }
+        )
+            .then(res => res.json())
+            .then(() => {
+
+            })
+
+    }
+
     return <>
         {
             filteredProducts.map(
                 product => {
-                    return <div>
+                    return <div key={`product--${product.id}`} className="product">
                         <h3>{product.name}</h3>
-                        <div>Price: ${product.price}</div>
+                        <div>Price: ${product.price}   <button onClick={purchaseButtonHandler(product.id)}>Purchase</button></div>
                     </div>
                 }
             )
